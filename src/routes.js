448b6243837102f0a0845cd1e3bb221cad590a60
Krisnaxda <krisna.amarta4c@gmail.com>
2020-01-09 17:11:38 +0700

    Route mode History

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import showBlogs from './components/showBlogs.vue';
 import addBlog from './components/addBlog.vue';
+import listBlogs from './components/listBlogs.vue';
 
 export default[
-  { path: '/', component: showBlogs},
+  { path: '/', component: showBlogs, listBlogs},
   { path: '/add', component: addBlog}
 ]
