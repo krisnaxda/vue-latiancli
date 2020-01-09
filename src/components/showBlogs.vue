56f249a69a3138724c6867a8fd8d5ead335bf759
Krisnaxda <krisna.amarta4c@gmail.com>
2020-01-09 12:09:22 +0700

    custom directives v-directivemu:argument ='value'

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="show-blogs">
+  <div v-theme:kolom="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div class="single-blog" v-for="blog in blogs">
-      <h2>{{blog.title}}</h2>
+      <h2 v-rainbow>{{blog.title}}</h2>
       <article>
         {{ blog. body}}
       </article>
